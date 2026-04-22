a496083075e4ebbd2b0a76f7cd4b8abaf66c4dc3
cbb1997 <cbd45068@gmail.com>
2026-04-21 23:22:54 -0700

    Started enemy ai + visual dict

--- FearFrog/Assets/Scripts/Game Logic/GameController.cs
+++ FearFrog/Assets/Scripts/Game Logic/GameController.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    [SerializeField] private VisualDictionary<string, int> m_Dict;
+
     private void Start()
     {
         Debugger.Log("Engine initialized...");
+
+        m_Dict = new VisualDictionary<string, int>(5);
     }
 }
